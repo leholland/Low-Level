845984290d5e60a25e34cffcda41f158d8bc8edb
Leah Holland <799@holbertonschool.com>
2019-06-20 06:29:59 +0000

    correcting spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - print size of different data type
- * Return: Always 0 (Success)
+* main - print size of different data type
+* Return: Always 0 (Success)
 **/
 int main(void)
 {
