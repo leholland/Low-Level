fec74a7644f9d5679dd2c69b704ec61e656bf295
Leah Holland <799@holbertonschool.com>
2019-06-20 06:44:39 +0000

    Correcting typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ char ca;
 printf("Size of a char: %ld byte(s)\n", sizeof(ca));
 printf("Size of an int: %ld byte(s)\n", sizeof(ia));
 printf("Size of a long int: %ld byte(s)\n", sizeof(iia));
-printf("Size of long long int: %ld byte(s)\n", sizeof(iiia));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(iiia));
 printf("Size of a float: %ld byte(s)\n", sizeof(fa));
 return (0);
 }
