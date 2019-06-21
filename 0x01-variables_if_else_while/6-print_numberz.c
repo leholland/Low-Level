bc4da99587e199be48ca4d0904af02fd33dc684c
Leah Holland <799@holbertonschool.com>
2019-06-21 04:02:43 +0000

    prints single base 10 numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+putchar(i % 10 + '0');
+i++;
+}
+putchar('\n');
+return (0);
+}
