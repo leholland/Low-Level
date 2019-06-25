f60d2efd5ba7ba5a5883068e6456a9ec2637e536
Leah Holland <799@holbertonschool.com>
2019-06-25 09:22:19 +0000

    using putchar to print alphabet, and print again but 10 times

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_alphabet - prints alphabet
+*
+*/
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+
+}
+
