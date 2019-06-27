be0089b2524459e01db6e65e8092045539155160
Leah Holland <799@holbertonschool.com>
2019-06-27 02:51:22 +0000

    print n times table starting at 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,44 @@
 #include "holberton.h"
 
-
+/**
+* print_times_table - displays n times table starting with 0
+* @n: starting integer for loop
+*
+*/
 void print_times_table(int n)
 {
 int i, j, k;
 
-if (n > 0 && n < 15)
+if (n > 15 || n < 0)
+return;
 
+i = 0;
+while (i <= n)
 {
+j = 0;
+while (j <= n)
+{
+k = i *j;
+if (k > 99)
+_putchar(k / 100 + '0');
+if (k > 9)
+_putchar((k / 10) % 10 + '0');
 
-for (j = 0; j <= n; j++)
-_putchar(
+_putchar(k % 10 + '0');
+
+if (j < n)
 {
- 
+_putchar(',');
+_putchar(' ');
+if (i * (j + 1) < 10)
+_putchar(' ');
+
+if (i * (j + 1) < 100)
+_putchar(' ');
+}
+++j;
+}
+_putchar('\n');
+++i;
+}
 }
