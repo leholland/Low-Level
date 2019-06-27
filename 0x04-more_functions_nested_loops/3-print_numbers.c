ee43fe8e064cace7ddfb7961dd4c4b128c4b8f39
Leah Holland <799@holbertonschool.com>
2019-06-27 23:44:42 +0000

    Add files for print numbers task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* print_numbers - displays numbers from 0-9
+*
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = '0' ; i <= '9' ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
