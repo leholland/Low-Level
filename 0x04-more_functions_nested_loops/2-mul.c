184daa4fc49fd395957af87afd0ba67e17dc0a3a
Leah Holland <799@holbertonschool.com>
2019-06-27 23:13:27 +0000

    Returning the sum of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* mul - multiplies two integers
+* @a: integer to be multiplied
+* @b: integer to be multiplied
+* Return: sum of a and b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
