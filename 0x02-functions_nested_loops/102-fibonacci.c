9f6db617cd92342049c033abbc621abae52fbbea
Leah Holland <799@holbertonschool.com>
2019-06-27 07:37:52 +0000

    Changing variables

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,17 @@
 int main(void)
 {
 long a, b, c;
-a = 1;
-b = 2;
-printf("%lu, ", a);
-printf("%lu, ", b);
+a = 0;
+b = 1;
+printf("%ld, ", b);
 for (int i = 0 ; i < 47; i++)
 {
 c = a + b;
-printf("%lu, ", c);
+printf("%ld, ", c);
 a = b;
 b = c;
 }
-printf("%lu\n", a + b);
+printf("%ld\n", a + b);
 
 return (0);
 }
