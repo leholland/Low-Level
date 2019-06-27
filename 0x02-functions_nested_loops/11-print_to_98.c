9c5220840cb8560bee6d94fdc13f739363c3b7b5
Leah Holland <799@holbertonschool.com>
2019-06-27 02:19:35 +0000

    Corrected spacing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,29 +2,45 @@
 #include <stdio.h>
 
 /**
- * print_to_98  - prints natural numbers from n to 98
- *
- */
+* print_to_98  - prints natural numbers from n to 98
+*
+*/
 void print_to_98(int n)
 {
-if (n == 99
+if (n > 98)
 {
-printf("%d\n", n);
+while (n >= 98)
+{
+printf("%d", n);
+
+if (n != 98)
+{
+putchar(',');
+putchar(' ');
 }
-else if (n < 99)
+
+n--;
+}
+}
+else if (n < 98)
 {
-while (n < 99)
+while (n <= 98)
 {
-printf("%d, ", n);
+printf("%d", n);
+
+if (n != 98)
+{
+putchar(',');
+putchar(' ');
+}
+
 n++;
 }
 }
 else
 {
-while (n > 98)
-{
-printf("%d, ", n);
-n--;
+printf("%d", n);
 }
-}
+
+putchar('\n');
 }
