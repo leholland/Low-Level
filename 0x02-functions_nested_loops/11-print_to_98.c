dc416aaa1c7130afad97c3dc298ed645736f60d0
Leah Holland <799@holbertonschool.com>
2019-06-27 02:27:43 +0000

    Corrected spacing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,40 +7,29 @@
 */
 void print_to_98(int n)
 {
-if (n > 98)
+if (n == 98)
 {
-while (n >= 98)
-{
-printf("%d", n);
-
-if (n != 98)
-{
-putchar(',');
-putchar(' ');
+printf("%d\n", 98);
 }
-
-n--;
+else if (n > 98)
+{
+for (; n > 97; n--)
+{
+if (n == 98)
+printf("%d\n", n);
+else
+printf("%d, ", n);
 }
 }
 else if (n < 98)
 {
-while (n <= 98)
+for (; n < 99; n++)
 {
-printf("%d", n);
-
-if (n != 98)
-{
-putchar(',');
-putchar(' ');
-}
-
-n++;
-}
-}
+if
+(n == 98)
+printf("%d\n", n);
 else
-{
-printf("%d", n);
+printf("%d, ", n);
 }
-
-putchar('\n');
+}
 }
