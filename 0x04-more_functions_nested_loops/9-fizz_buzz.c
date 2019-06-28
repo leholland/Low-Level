590d215dc9b30d18d79a5a6974105f8504d15478
Leah Holland <799@holbertonschool.com>
2019-06-28 04:19:40 +0000

    Fixes logic

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,27 +13,17 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 15 == 0)
-{
-printf("FizzBuzz");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else
-{
+if (i % 5!= 0 && i % 3 != 0)
 printf("%d", i);
-}
+else if (i % 5 == 0 && i % 3 == 0)
+printf("FizzBuzz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("Fizz");
 if (i != 100)
-{
-    printf(" ");
+putchar(' ');
 }
-}
-printf("\n");
+putchar('\n');
 return (0);
 }
