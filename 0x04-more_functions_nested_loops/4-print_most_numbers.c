2b1f9d6439731770d4d98747a7ae6b1396b14048
Leah Holland <799@holbertonschool.com>
2019-06-28 00:05:30 +0000

    Prints 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - Displays number 0-9 excludi* ng 2 and 4
+*
+*/
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = '0' ; i <= '9' ; i++)
+{
+if (i != '2' && i != '4')
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
