43f4abe3a8ba72bf5ac771d9bad8a80330f18414
Leah Holland <799@holbertonschool.com>
2019-07-03 03:19:49 +0000

    fixing errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,10 +3,9 @@
 /**
 * _strcpy - Copies string pointed by src including null byte
 * @dest: Destination of copy
-* @char: Var
 * @src: Source of copy
+* Return: Copy of the source string
 */
-
 char *_strcpy(char *dest, char *src)
 {
 int counter, i;
@@ -14,9 +13,15 @@ int counter, i;
 counter = 0;
 
 while (src[counter] != '\0')
+{
 counter++;
+}
+
 for (i = 0; i < counter; i++)
+{
 dest[i] = src[i];
+}
+
 dest[counter] = '\0';
 
 return (dest);
