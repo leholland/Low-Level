06993184e439dc447e38a0b482781294b302929d
Leah Holland <799@holbertonschool.com>
2019-07-10 06:13:42 +0000

    searches a string for any set of bytes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,16 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j, k = 0
+unsigned int i, j, k = 0;
 for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[i] != '\0'; j++)
-{
-if (k[i] == accept[j])
-{
-k++;
-break;
-}
-}
-
-if (s[i] != accept[j])
-break;
-}
-return (k);
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (k != 0 && (s[i] == ' ' || s[i] == ',')) 
+                return (k);
+            else if (s[i] == accept[j])
+            k++;
+        }
+    }
+    return (k);
 }
