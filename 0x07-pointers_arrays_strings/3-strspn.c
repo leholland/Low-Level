005ffb57695bddf86aaa24e97723ff8cb8949f65
Leah Holland <799@holbertonschool.com>
2019-07-10 18:53:12 +0000

    Removing trailing and leading whitespace

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,14 +11,14 @@ unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j, k = 0;
 for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (k != 0 && (s[i] == ' ' || s[i] == ',')) 
-                return (k);
-            else if (s[i] == accept[j])
-            k++;
-        }
-    }
-    return (k);
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (k != 0 && (s[i] == ' ' || s[i] == ','))
+return (k);
+else if (s[i] == accept[j])
+k++;
+}
+}
+return (k);
 }
