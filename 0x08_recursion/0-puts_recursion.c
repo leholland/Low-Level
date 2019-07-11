bca8921e1f7bfadb0272d7a71acab74e958c413f
Leah Holland <799@holbertonschool.com>
2019-07-11 00:50:41 +0000

    Prints a string recursively

---
+++ 0x08_recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - Prints a string recursively
+*@s: String to be printed
+*
+*/
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+s++;
+_puts_recursion(s);
+}
