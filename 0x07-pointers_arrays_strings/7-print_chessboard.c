55417ecec44919432f148658e61f6a855c303b0f
Leah Holland <799@holbertonschool.com>
2019-07-11 04:33:31 +0000

    Print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 
 /**
-* print_chessboard - Function to print chessboard 
-*@a: array to print 
+* print_chessboard - Function to print chessboard
+*@a: array to print
 *
 */
 
 void print_chessboard(char (*a)[8])
 {
- int i, j;
- i = j = 0;
+int i, j;
+i = j = 0;
 
- while (i < 8)
- {
-     j = 0;
-     while (j < 8)
+while (i < 8)
+{
+j = 0;
+while (j < 8)
 
-     {
-     _putchar(a[i][j]);
-     j++;
- }
- _putchar('\n');
- i++;
+{
+_putchar(a[i][j]);
+j++;
+}
+_putchar('\n');
+i++;
 }
 }
