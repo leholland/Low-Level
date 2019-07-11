c74e5b619a2b3c2ba54ee421d9788653493612c9
Leah Holland <799@holbertonschool.com>
2019-07-11 04:58:41 +0000

    Add solution for print diagonal sums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - sums of two diags of square matrix of integers
+*@a: 
+*@size: Size o
+*/
+
+void print_diagsums(int *a, int size)
+{
+    int sum_1 = 0; 
+    int start_1 = 0;
+    int end_1 = size;
+
+    int sum_2 = 0;
+    int start_2 = size - 1;
+    int end_2 = size - 1;
+
+    while (size--)
+    {
+        sum_1 += a[start_1];
+        start_1 += (end_1 + 1);
+        sum_2 += a[start_2];
+        start_2 += end_2;
+    }
+
+    printf("%d, %d\n", sum_1, sum_2);
+}
