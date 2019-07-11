b56da04de4e7376c0cfeb9c71163c79039188309
Leah Holland <799@holbertonschool.com>
2019-07-11 04:26:43 +0000

    Add set string solution

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+* set_string - Sets the value of s to a char
+*@s: address of pointer to point to char*
+*@to: point to
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
