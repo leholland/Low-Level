d5d3a74c55bc9e7b1584adc8fa86e99a11b449d0
Leah Holland <799@holbertonschool.com>
2019-07-11 02:55:27 +0000

    Removing helper function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,27 +1,19 @@
 #include "holberton.h"
 
 /**
-* _strlen - Helper function
-*@s: Pointer to determine size of
-*@len: Length of string
-* Return: strlen, s, len
-*/
-
-int _strlen(char *s, int len)
-{
-if (*s == '\0')
-{
-return (len);
-{
-return (_strlen(++s, ++len));
-}
-/**
 * _strlen_recursion - Returns length of string
-*@s: Pointer to determine size of
-* Return: _strlen, s, 0
+*@s: String to determine length of
+* Return: Length of string s
 */
 
 int _strlen_recursion(char *s)
 {
-return (_strlen(s, 0));
+if (*s == '\0')
+{
+return 0;
+}
+else
+{
+return 1 + _strlen_recursion(s + 1);
+}
 }
