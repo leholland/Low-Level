0ba0a0571212742b71fab2f173f282ac1ad1264e
Leah Holland <799@holbertonschool.com>
2019-07-11 04:59:42 +0000

    Add solution for print diagonal sums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,21 +9,21 @@
 
 void print_diagsums(int *a, int size)
 {
-    int sum_1 = 0; 
-    int start_1 = 0;
-    int end_1 = size;
+int sum_1 = 0; 
+int start_1 = 0;
+int end_1 = size;
 
-    int sum_2 = 0;
-    int start_2 = size - 1;
-    int end_2 = size - 1;
+int sum_2 = 0;
+int start_2 = size - 1;
+int end_2 = size - 1;
 
-    while (size--)
-    {
-        sum_1 += a[start_1];
-        start_1 += (end_1 + 1);
-        sum_2 += a[start_2];
-        start_2 += end_2;
-    }
+while (size--)
+{
+sum_1 += a[start_1];
+start_1 += (end_1 + 1);
+sum_2 += a[start_2];
+start_2 += end_2;
+}
 
-    printf("%d, %d\n", sum_1, sum_2);
+printf("%d, %d\n", sum_1, sum_2);
 }
