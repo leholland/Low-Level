c2cb90e5e7d970c2b613f982a89609ebe7ac23cc
Leah Holland <799@holbertonschool.com>
2019-07-22 23:16:37 +0000

    Fixing spaces

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,25 +8,10 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
+if (d)
 {
-return;
-}
-if (d->name == NULL)
-{
-printf("Name: (nil)\n");
-}
-else
-{
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-}
-if (d->owner == NULL)
-{
-printf("Owner: (nil)\n");
-}
-else
-{
-printf("Owner: %s\n", d->owner);
+printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
 }
