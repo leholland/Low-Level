76294211cb00d4ae9a51c35e22afbbac75acaed4
Leah Holland <799@holbertonschool.com>
2019-07-25 01:10:10 +0000

    Op functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Calculates integers
+* @argc: Contains 4
+* @argv: Stores values
+* Return: Result of calculated integers
+*/
+
+int main(int argc, char **argv)
+{
+int a;
+int b;
+int result;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+{
+printf("Error\n");
+exit(100);
+}
+
+if (argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+
+result = get_op_func(argv[2])(a, b);
+printf("%d\n", result);
+return(0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - Adds two integers
+* @a: First int
+* @b: Second int
+* Return: Sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+return ((a) + (b));
+}
+
+/**
+* op_sub - Subtracts two integers
+* @a: First int
+* @b: Second int
+* Return: Returns subtracted numbers
+*/
+
+int op_sub(int a, int b)
+{
+return ((a) - (b));
+}
+
+/**
+* op_mul - Multiplies two integers
+* @a: First int
+* @b: Second int
+* Return: Multiplied numbers
+*/
+
+int op_mul(int a, int b)
+{
+return ((a) * (b));
+}
+
+/**
+* op_div - Divides two integers
+* @a: First int
+* @b: Second int
+* Return: Divided numbers
+*/
+
+int op_div(int a, int b)
+{
+return ((a) / (b));
+}
+
+/**
+* op_mod - Mods two integers
+* @a: First int
+* @b: Second int
+* Return: Returns modded ints
+*/
+
+int op_mod(int a, int b)
+{
+return ((a) % (b));
+}
