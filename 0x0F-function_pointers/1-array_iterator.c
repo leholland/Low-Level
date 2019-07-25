4ff4878e9c24e0214365fcd7330d5485ea03b0a2
Leah Holland <799@holbertonschool.com>
2019-07-25 00:18:40 +0000

    Executes function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - Executes function given as param on each elem of array
+* @array: Pointer to array
+* @size: Size of array
+* @actoin: Funct
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i = 0;
+
+if (array && action)
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
